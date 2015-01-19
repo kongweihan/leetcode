681a892fd68ad7c0c41a36d2db52ac4754c1d506
Weihan Kong <kongweihan@users.noreply.github.com>
2015-01-18 23:10:51 -0800

    Create Implement strStr().java

---
+++ Implement strStr().java
@@ -0,0 +1,15 @@
+public class Solution {
+    public int strStr(String haystack, String needle) {
+        for (int i = 0; i <= haystack.length() - needle.length(); i++) {
+            boolean found = true;
+            for (int j = 0; j < needle.length(); j++) {
+                if (needle.charAt(j) != haystack.charAt(i + j)) {
+                    found = false;
+                    break;
+                }
+            }
+            if (found) return i;
+        }
+        return -1;
+    }
+}
